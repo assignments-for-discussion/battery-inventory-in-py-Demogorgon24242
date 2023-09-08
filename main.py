4c840871aafbc61c376f933f9bc052892c1d1352
Aditya Kumar Choubey <choubeyaditya123@gmail.com>
2023-09-08 19:15:21 +0530

    First Commit with functions implemented

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 
+def soh_calc(capacity):
+  rated_capacity= 120.0
+  soh_vals=[]
+  for i in capacity:
+    soh=  100 * (i/ rated_capacity)
+    soh_vals.append(soh)
+  return soh_vals
+
+
+
 def count_batteries_by_health(present_capacities):
+  soh = soh_calc(present_capacities)
+  health=0
+  exchange=0
+  fail=0
+  for i in soh:
+    if i>80.00 :
+      health+=1
+    elif i<=80.00 and i>65.00:
+      exchange+=1
+    else:
+      fail+=1
   return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
+    "healthy": health,
+    "exchange": exchange,
+    "failed": fail
   }
 
 
